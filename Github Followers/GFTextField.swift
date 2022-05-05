ea81beb079489d0cf309306461b59baeb8a98aeb
mfsaglam <mfsaglam1@gmail.com>
2022-05-05 18:09:13 +0300

    initial setup

---
+++ Github Followers/GFTextField.swift
@@ -0,0 +1,21 @@
+//
+//  GFTextField.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 2.05.2022.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
