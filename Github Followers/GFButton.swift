733840327f505f67a78cc21b47c81418c56e8312
mfsaglam <mfsaglam1@gmail.com>
2022-05-06 11:15:33 +0300

    add GFButton and GFTextfield

---
+++ Github Followers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 6.05.2022.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(color: UIColor, title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        backgroundColor = color
+        configure() 
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
