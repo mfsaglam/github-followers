18d78b5ac41cb1dbdce5dddb4d3ba968322ca742
mfsaglam <mfsaglam1@gmail.com>
2022-05-07 11:58:36 +0300

    create GFAlertVC

---
+++ Github Followers/GFBodyLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFBodyLabel.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 7.05.2022.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(alignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = alignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
