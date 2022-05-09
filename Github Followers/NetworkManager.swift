92d3af47cd428ced3e16f54de0bed20c717487f4
mfsaglam <mfsaglam1@gmail.com>
2022-05-09 20:15:24 +0300

    create network manager

---
+++ Github Followers/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 9.05.2022.
+//
+
+import Foundation
+
+class NetworkManager {
+    let shared = NetworkManager()
+}
