db7c763e6e1302e2608e4512f394c91c1ba2cb3e
mfsaglam <mfsaglam1@gmail.com>
2022-05-12 18:35:47 +0300

    add avatarImageView and usenameLabel for FollowerCell

--- Github Followers/Custom Views/Cells/FollowerCell.swift
+++ Github Followers/Custom Views/Cells/FollowerCell.swift
@@ -9,4 +9,7 @@ import UIKit
 
 class FollowerCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
+    
+    let avatarImageView = UIImageView()
+    let usenameLabel = GFTitleLabel(alignment: .left, fontSize: 16)
 }
