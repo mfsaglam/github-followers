a5a331b2a3e79894c7d48f5a733517ec99f2266f
mfsaglam <mfsaglam1@gmail.com>
2022-05-19 15:17:07 +0300

    download images

--- Github Followers/Custom Views/Cells/FollowerCell.swift
+++ Github Followers/Custom Views/Cells/FollowerCell.swift
@@ -24,6 +24,23 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower) {
         usernameLabel.text = follower.login
+        downloadImage(from: follower.avatarUrl)
+    }
+    
+    func downloadImage(from url: String) {
+        guard let url = URL(string: url) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async { self.avatarImageView.image = image }
+        }
+
+        task.resume()
     }
     
     private func configure() {
