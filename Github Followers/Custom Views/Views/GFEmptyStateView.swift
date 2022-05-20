d1362bf06502558ea178cad594bf8e83197dff3d
mfsaglam <mfsaglam1@gmail.com>
2022-05-20 18:21:43 +0300

    fully create emotyStateView

--- Github Followers/Custom Views/Views/GFEmptyStateView.swift
+++ Github Followers/Custom Views/Views/GFEmptyStateView.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class GFEmptyStateView: UIView {
     
-    let message = GFTitleLabel(alignment: .center, fontSize: 28)
-    let image = UIImageView()
+    let messageLabel = GFTitleLabel(alignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,14 +22,31 @@ class GFEmptyStateView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    init(message: String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
     func configure() {
-        addSubview(message)
-        addSubview(image)
+        addSubview(messageLabel)
+        addSubview(logoImageView)
         
-        message.numberOfLines = 3
-        message.textColor = .secondaryLabel
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
         
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        ])
     }
 }
