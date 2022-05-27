a785028eb63c43b81f39cc8624591df40d8746d9
mfsaglam <mfsaglam1@gmail.com>
2022-05-27 18:07:31 +0300

    make successful network call for user

--- Github Followers/Screens/UserInfoVC.swift
+++ Github Followers/Screens/UserInfoVC.swift
@@ -16,10 +16,21 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        print(username!)
+        getUser()
     }
     
     @objc func dismissVC() {
         dismiss(animated: true)
+    }
+    
+    func getUser() {
+        NetworkManager.shared.getUser(for: username) { result in
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "ok")
+            }
+        }
     }
 }
