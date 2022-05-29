e47a02aefafc8b9fe2feae42c143d247fc86b95e
mfsaglam <mfsaglam1@gmail.com>
2022-05-29 23:08:33 +0300

    clean userInfoVC

--- Github Followers/Screens/UserInfoVC.swift
+++ Github Followers/Screens/UserInfoVC.swift
@@ -14,15 +14,19 @@ class UserInfoVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
-        navigationItem.rightBarButtonItem = doneButton
-        getUser()
+        configureViewController()
         configureUI()
+        getUser()
     }
     
     @objc func dismissVC() {
         dismiss(animated: true)
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
     }
     
     func getUser() {
