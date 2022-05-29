70683bf42a5607f9197335cee4cab40e740dea8f
mfsaglam <mfsaglam1@gmail.com>
2022-05-29 23:11:51 +0300

    create GFItemInfoView file

---
+++ Github Followers/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 29.05.2022.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
