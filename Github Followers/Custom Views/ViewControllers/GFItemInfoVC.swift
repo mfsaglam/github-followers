0a95937e9c46cceecd216eb6f85202283ac6a8b9
mfsaglam <mfsaglam@yandex.com>
2022-06-02 00:50:14 +0300

    create GFItemInfoVC file

---
+++ Github Followers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,16 @@
+//
+//  GFItemInfoVC.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 2.06.2022.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
