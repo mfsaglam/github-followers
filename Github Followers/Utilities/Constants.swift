b046bf855acee38e3d854959d2c5cb61a5498d20
mfsaglam <mfsaglam@yandex.com>
2022-06-02 00:30:40 +0300

    create constants file and add constants

---
+++ Github Followers/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Github Followers
+//
+//  Created by Fatih Sağlam on 2.06.2022.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let locations = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
+}
