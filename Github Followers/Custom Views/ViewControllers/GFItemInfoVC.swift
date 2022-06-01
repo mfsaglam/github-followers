93908f415d096e3aa2bd338e23b6a642e7f2e0a3
mfsaglam <mfsaglam@yandex.com>
2022-06-02 01:44:08 +0300

    add constraints

--- Github Followers/Custom Views/ViewControllers/GFItemInfoVC.swift
+++ Github Followers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -27,12 +27,22 @@ class GFItemInfoVC: UIViewController {
     
     private func layoutUI() {
         view.addSubview(stackView)
-        view.addSubview(itemInfoViewOne)
-        view.addSubview(itemInfoViewTwo)
         view.addSubview(actionButton)
         
-        NSLayoutConstraint.activate([
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
 }
