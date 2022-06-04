716e71582d8e36c0f20e18a613561bd5aab69d12
mfsaglam <mfsaglam@yandex.com>
2022-06-05 00:04:22 +0300

    populate text

--- Github Followers/Screens/UserInfoVC.swift
+++ Github Followers/Screens/UserInfoVC.swift
@@ -43,6 +43,7 @@ class UserInfoVC: UIViewController {
                     self.add(childVC: userInfoHeaderView, to: self.headerView)
                     self.add(childVC: GFRepoItemVC(user: user), to: self.itemViewOne)
                     self.add(childVC: GFFollowerItemVC(user: user), to: self.itemViewTwo)
+                    self.dateLabel.text = user.createdAt.convertToDate()?.convertToMonthYearFormat()
                 }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "ok")
