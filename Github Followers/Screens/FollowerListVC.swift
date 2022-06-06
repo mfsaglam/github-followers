32bd6a46719643fa13caadf21c3476d9e70cdd86
mfsaglam <mfsaglam@yandex.com>
2022-06-07 00:20:38 +0300

    display followers

--- Github Followers/Screens/FollowerListVC.swift
+++ Github Followers/Screens/FollowerListVC.swift
@@ -154,6 +154,12 @@ extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
 
 extension FollowerListVC: FollowerListVCDelegate {
     func didRequestFollowers(for username: String) {
-        //fill the page
+        self.username = username
+        title = username 
+        page = 1
+        filteredFollowers.removeAll()
+        followers.removeAll()
+        collectionView.setContentOffset(.zero, animated: true)
+        getFollowers(username: username, page: page)
     }
 }
