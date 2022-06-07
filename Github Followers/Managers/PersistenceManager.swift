06b3bbcf931ee1299927d886ef864183ba9c500f
mfsaglam <mfsaglam@yandex.com>
2022-06-07 22:17:42 +0300

    decode favorites

--- Github Followers/Managers/PersistenceManager.swift
+++ Github Followers/Managers/PersistenceManager.swift
@@ -15,8 +15,17 @@ enum PersistenceManager {
     static private let defaults = UserDefaults.standard
     
     static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
-        guard let favoritesData  = defaults.object(forKey: Keys.favorites ) else {
-            
+        guard let favoritesData  = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.invalidData))
         }
     }
 }
