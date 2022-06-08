4a019016298c504044d5debad578ea0eb4779727
mfsaglam <mfsaglam@yandex.com>
2022-06-08 22:43:46 +0300

    save the data

--- Github Followers/Managers/PersistenceManager.swift
+++ Github Followers/Managers/PersistenceManager.swift
@@ -30,8 +30,10 @@ enum PersistenceManager {
                         return
                     }
                     retrievedFavorites.append(favorite)
+                    completed(save(favorites: retrievedFavorites))
                 case .remove:
                     retrievedFavorites.removeAll { $0.login == favorite.login }
+                    completed(save(favorites: retrievedFavorites))
                 }
             case .failure(let error):
                 completed(error)
