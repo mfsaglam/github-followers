11313aeff3ddc51e8b8bf3ad2411e14c6605c3b1
mfsaglam <mfsaglam@yandex.com>
2022-06-10 00:22:06 +0300

    clear viewDidLoad

--- Github Followers/Screens/FavoritesListVC.swift
+++ Github Followers/Screens/FavoritesListVC.swift
@@ -11,6 +11,17 @@ class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        getFavorites()
+    }
+    
+    func configure() {
+        view.backgroundColor = .systemBackground
+        title = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func getFavorites() {
         PersistenceManager.retrieveFavorites { result in
             switch result {
             case .success(let favorites):
