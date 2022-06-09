65f5ec71f5321171bb22b27ce1c885b2f40f76ec
mfsaglam <mfsaglam@yandex.com>
2022-06-09 23:06:22 +0300

    add configure method

--- Github Followers/Custom Views/Cells/FavoriteCell.swift
+++ Github Followers/Custom Views/Cells/FavoriteCell.swift
@@ -16,10 +16,15 @@ class FavoriteCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        
     }
     
 }
